3e84c0848e4359cf1412e7a756af9a8f9a073d71
Djfls <Ak38vhfO@proton.me>
2024-04-11 15:24:31 +0900

    データバインディング

---
+++ source/repos/Maui/WpfApp2/WpfApp2/UserControl4.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using static WpfApp2.UserControl4;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// UserControl4.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UserControl4 : UserControl
+    {
+        public UserControl4()
+        {
+            Brush brush = new SolidColorBrush(Colors.YellowGreen);
+            InitializeComponent();
+            b.Background = brush;
+
+            Binding binding = new Binding();
+            binding.Source = t1;
+            binding.Path = new PropertyPath("Text");
+            cc1.SetBinding(ContentControl.ContentProperty, binding);
+            DataContext = new User4ViewModel();
+
+        }
+
+        private void b3_Click(object sender, RoutedEventArgs e)
+        {
+            Brush brush = new SolidColorBrush(Colors.Blue);
+            this.Resources["brush"] = brush;
+            b.SetResourceReference(Button.BackgroundProperty, "brush");
+
+            //this.FindResource("brush") = brush;
+        }
+    }
+
+    public class User4ViewModel 
+    {
+        public Human MyHuman { get; set; } = new Human { Name ="Tanaka"};
+    }
+
+    public class Human
+    {
+        public string Name { get; set; }
+    }
+
+    public class HumanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Human)value).Name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Human h = new Human();
+            h.Name = value.ToString();
+            return h;
+        }
+    }
+
+}
---
+++ source/repos/Maui/WpfApp2/WpfApp2/UserControl5.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// UserControl5.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UserControl5 : UserControl
+    {
+        public UserControl5()
+        {
+            InitializeComponent();
+
+            //XmlDocument doc = new XmlDocument();
+            //doc.LoadXml(
+            //    @"
+            //    <Media xmlns="""">
+            //        <Book Author=""山田"" Title=""愛は天国""/>
+            //        <Book Author=""佐藤"" Title=""愛は地獄""/>
+            //        <Book Author=""鈴木"" Title=""愛は生きる糧""/>
+            //        <CD Artist=""山田"" Title=""愛子は歌う""/>
+            //        <DVD Director=""山田"" Title=""愛という名の映画"">
+            //            <Actor>佐藤</Actor>
+            //            <Actor>鈴木</Actor>
+            //        </DVD>
+            //    </Media>"
+            //    );
+
+            //FrameworkElementFactory frameworkElementFactory = new FrameworkElementFactory(typeof(TextBlock));
+            //Binding bind = new Binding();
+            //bind.XPath = "@Title";
+            //frameworkElementFactory.SetBinding(TextBlock.TextProperty, bind);
+
+            //DataTemplate template = new DataTemplate();
+            //template.DataType = typeof(XmlElement);
+            //template.VisualTree = frameworkElementFactory;
+
+            //XmlDataProvider xmlDataProvider = new XmlDataProvider();
+            //xmlDataProvider.Document = doc;
+            //bind = new Binding();
+            //bind.Source = doc;
+            //bind.XPath = "\\Media\\Book";
+            //ListBox listBox = new ListBox();
+            //listBox.ItemTemplate = template;
+            //listBox.SetBinding(ListBox.ItemsSourceProperty, bind);
+            //Content = listBox;
+
+            //DataContext = xmlDataProvider;
+
+        }
+    }
+}
